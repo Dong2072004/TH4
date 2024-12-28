de47d803b1c605ab81c25b8b0b68d751b3ca7347
Tran Tien Dat <77674466+tida-tr@users.noreply.github.com>
2024-12-28 17:55:40 +0700

    Update ProductController.php

--- cau2/cau2_bth4/app/Http/Controllers/ProductController.php
+++ cau2/cau2_bth4/app/Http/Controllers/ProductController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    
+    public function index() {
+        $products = Product::all();
+        return view('product.index', compact('products'));
+    }
+
+    public function create() {
+
+    }
+
+    public function store(Request $request) {
+
+    }
+
+    public function show($id) {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function destroy($id) {
+
+    }
 }
