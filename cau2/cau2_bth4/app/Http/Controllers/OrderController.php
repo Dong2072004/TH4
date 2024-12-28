2c348525056923638171857fad8ef503ebb9d703
Tran Tien Dat <77674466+tida-tr@users.noreply.github.com>
2024-12-28 17:55:15 +0700

    Update OrderController.php

--- cau2/cau2_bth4/app/Http/Controllers/OrderController.php
+++ cau2/cau2_bth4/app/Http/Controllers/OrderController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index() {
+        $orders = Order::all();
+        return view('orders.index', compact('orders'));
+    }
+
+    public function create() {
+
+    }
+
+    public function store(Request $request) {
+
+    }
+
+    public function show($id) {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function destroy($id) {
+
+    }
 }
