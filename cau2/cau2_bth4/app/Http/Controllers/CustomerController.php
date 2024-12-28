8f6c460c2534984f1f2f4dcc782508313935a9f8
Tran Tien Dat <77674466+tida-tr@users.noreply.github.com>
2024-12-28 17:54:52 +0700

    Update CustomerController.php

--- cau2/cau2_bth4/app/Http/Controllers/CustomerController.php
+++ cau2/cau2_bth4/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    public function index() {
+        $customers = Customer::all();
+        return view('customer.index', compact('customers'));
+    }
+
+    public function create() {
+
+    }
+
+    public function store(Request $request) {
+
+    }
+
+    public function show($id) {
+
+    }
+
+    public function edit($id) {
+
+    }
+
+    public function update(Request $request, $id) {
+
+    }
+
+    public function destroy($id) {
+
+    }
+
+
 }
